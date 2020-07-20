4c2bd722cc88de963d78a4611501f889a53ef214
moriyamasaaaki <moriya-7071@outlook.com>
2020-07-20 18:55:08 +0900

    feat(App,SudeNav, index): サイドメニュー追加と開閉処理 #2

---
+++ src/components/SideNav.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-sheet>
+    <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="/img/user-img.png"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>守屋 雅章</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [{ title: "プロフィール", icon: "mdi-account" }]
+    };
+  }
+};
+</script>
