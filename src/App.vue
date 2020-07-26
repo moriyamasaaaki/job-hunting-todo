f7fcbb2796c51e743795328551f353f963147c14
moriyamasaaaki <moriya-7071@outlook.com>
2020-07-26 20:05:52 +0900

    refactor(App,Header): コンポーネント分割（Header) #25

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon @click.stop="toggleSideMenu"></v-app-bar-nav-icon>
-      <router-link to="/">
-        <span>就活ToDo</span>
-      </router-link>
-      <v-spacer></v-spacer>
-      <div class="my-2">
-        <div v-if="!$store.state.login_user">
-          <v-btn @click="login">ログイン</v-btn>
-        </div>
-        <div v-else>
-          <v-btn @click="logout">ログアウト</v-btn>
-        </div>
-      </div>
-    </v-app-bar>
+    <Header />
     <SideNav />
     <v-snackbar v-model="snackbar">{{ text }}</v-snackbar>
     <v-content>
@@ -24,14 +10,14 @@
 </template>
 
 <script>
-import firebase from "firebase";
-import { mapActions } from "vuex";
 import SideNav from "@/components/SideNav";
+import Header from "@/components/Header";
 export default {
   name: "App",
 
   components: {
-    SideNav
+    SideNav,
+    Header
   },
 
   data: () => ({
@@ -39,41 +25,6 @@ export default {
     text: "ログアウトしました。"
   }),
 
-  created() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setLoginUser(user);
-        this.fetchTodos();
-      } else {
-        this.logoutUser();
-      }
-    });
-  },
-
-  methods: {
-    login() {
-      const google_auth = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithRedirect(google_auth);
-    },
-    logout() {
-      firebase.auth().signOut();
-      this.snackbar = true;
-    },
-    ...mapActions([
-      "toggleSideMenu",
-      "setLoginUser",
-      "logoutUser",
-      "fetchTodos"
-    ])
-  }
+  methods: {}
 };
 </script>
-
-<style lang="scss" scoped>
-a {
-  text-decoration: none;
-}
-span {
-  color: #fff;
-}
-</style>
