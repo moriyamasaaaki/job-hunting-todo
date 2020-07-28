b0b7ee7529a9e0f4e0a64e8849cf7546d96f30b4
moriyamasaaaki <moriya-7071@outlook.com>
2020-07-29 01:01:06 +0900

    feat(Todos.vue): Todo一覧ページ作成 #52

---
+++ src/views/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todos">
+    <TodoList />
+    <Modal />
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+import Modal from "@/components/Modal";
+export default {
+  name: "Todos",
+  components: {
+    TodoList,
+    Modal
+  }
+};
+</script>
